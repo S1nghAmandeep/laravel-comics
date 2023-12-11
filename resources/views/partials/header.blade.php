de659fbc0bb3b051e52212766d8fb562fca94cca
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-11 19:17:05 +0100

    add a tag

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,18 +2,18 @@
     <div class="container">
         <div class="row">
             <ul class="nav-bar">
-                <li><img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt=""></li>
-                <li>Characters</li>
-                <li>Comics</li>
-                <li>Movies</li>
-                <li>Tv</li>
-                <li>Games</li>
-                <li>Collectibles</li>
-                <li>Videos</li>
-                <li>Fans</li>
-                <li>News</li>
-                <li>Shop</li>
-                <li><input type="text"></li>
+                <li><img class="logo" src="{{Vite::asset('resources/img/dc-logo.png')}}" alt=""></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><input type="text" placeholder="&#x1F50D; Search "></li>
             </ul>
         </div>
     </div>
