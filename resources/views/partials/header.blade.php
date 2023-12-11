a96f4b7b03dc37632ea3fe2e58417504ef61248a
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-11 15:59:49 +0100

    done header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <ul class="nav-bar">
+                <li><img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt=""></li>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+                <li><input type="text"></li>
+            </ul>
+        </div>
+    </div>
+</header>
