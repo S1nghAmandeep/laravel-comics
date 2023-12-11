7dcc35dceb38de66702847e8cd832b0563ab10fb
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-11 17:44:32 +0100

    add-footer text

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,51 @@
+<footer>
+    <div class="footer" style="background-image: url( {{Vite::asset('resources/img/footer-bg.jpg')}} )">
+        <div class="container text-light">
+            <div class="d-flex gap-5">
+                <div class="d-flex flex-column gap-2">
+                    <h5>Dc comics</h5>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <h5>Shop</h5>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collections</li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 class="pb-2">Dc</h5>
+                    <ul>
+                        <li>Terms of use</li>
+                        <li>Privacy</li>
+                        <li>Ad Chioces</li>
+                        <li>Add</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                        <li>lorem</li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 class="pb-2">Sites</h5>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
