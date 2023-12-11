77c0b287f367115db1d2a9b78c3ab6b9b7f4a07f
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-11 19:16:58 +0100

    add a tag

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,48 +1,70 @@
 <footer>
     <div class="footer" style="background-image: url( {{Vite::asset('resources/img/footer-bg.jpg')}} )">
-        <div class="container text-light">
-            <div class="d-flex gap-5">
-                <div class="d-flex flex-column gap-2">
-                    <h5>Dc comics</h5>
-                    <ul>
-                        <li>Characters</li>
-                        <li>Comics</li>
-                        <li>Movies</li>
-                        <li>Tv</li>
-                        <li>Games</li>
-                        <li>Videos</li>
-                        <li>News</li>
-                    </ul>
-                    <h5>Shop</h5>
-                    <ul>
-                        <li>Shop DC</li>
-                        <li>Shop DC Collections</li>
-                    </ul>
+        <div class="container text-light ">
+            <div class="d-flex justify-content-between">
+                <div class="d-flex gap-5">
+                    <div class="d-flex flex-column gap-2">
+                        <h5 class="text-uppercase">Dc comics</h5>
+                        <ul class="pb-2">
+                            <a href="#"><li>Characters</li></a>
+                            <a href="#"><li>Comics</li></a>
+                            <a href="#"><li>Movies</li></a>
+                            <a href="#"><li>Tv</li></a>
+                            <a href="#"><li>Games</li></a>
+                            <a href="#"><li>Videos</li></a>
+                            <a href="#"><li>News</li></a>
+                        </ul>
+                        <h5 class="text-uppercase">Shop</h5>
+                        <ul>
+                            <a href="#"><li>Shop DC</li></a>
+                            <a href="#"><li>Shop DC Collections</li></a>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5 class="pb-2 text-uppercase">Dc</h5>
+                        <ul>
+                            <a href="#"><li>Terms of use</li></a>
+                            <a href="#"><li>Privacy</li></a>
+                            <a href="#"><li>Ad Chioces</li></a>
+                            <a href="#"><li>Add</li></a>
+                            <a href="#"><li>lorem</li></a>
+                            <a href="#"><li>lorem</li></a>
+                            <a href="#"><li>lorem</li></a>
+                            <a href="#"><li>lorem</li></a>
+                            <a href="#"><li>lorem</li></a>
+                            <a href="#"><li>lorem</li></a>
+                            <a href="#"><li>lorem</li></a>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5 class="pb-2 text-uppercase">Sites</h5>
+                        <ul>
+                            <a href="#"><li>DC</li></a>
+                            <a href="#"><li>MAD Magazine</li></a>
+                            <a href="#"><li>DC Kids</li></a>
+                            <a href="#"><li>DC Universe</li></a>
+                            <a href="#"><li>DC Power Visa</li></a>
+                        </ul>
+                    </div>
                 </div>
                 <div>
-                    <h5 class="pb-2">Dc</h5>
-                    <ul>
-                        <li>Terms of use</li>
-                        <li>Privacy</li>
-                        <li>Ad Chioces</li>
-                        <li>Add</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                        <li>lorem</li>
-                    </ul>
+                    <img class="dc-logo" src="{{vite::asset('resources/img/dc-logo-bg.png')}}" alt="">
                 </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-social">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+                <button class="btn btn-outline-primary text-uppercase">Sign-up now!</button>
                 <div>
-                    <h5 class="pb-2">Sites</h5>
-                    <ul>
-                        <li>DC</li>
-                        <li>MAD Magazine</li>
-                        <li>DC Kids</li>
-                        <li>DC Universe</li>
-                        <li>DC Power Visa</li>
+                    <ul class="d-flex gap-2 align-items-center">
+                        <li><h5 class="text-primary me-2 mb-0 p-0 text-uppercase">Follow us</h5></li>
+                        <li><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt=""></li>
+                        <li><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt=""></li>
+                        <li><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt=""></li>
+                        <li><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt=""></li>
+                        <li><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt=""></li>
                     </ul>
                 </div>
             </div>
